8b61323efe72cc7b8906d00a7626ef074e9998d1
jicg <284077319@qq.com>
2020-07-20 11:31:53 +0800

    升级一些第三方包，去掉jpda_plugin

--- jpda_plugin/ios/Classes/JpdaPlugin.m
+++
@@ -1,8 +0,0 @@
-#import "JpdaPlugin.h"
-#import <jpda_plugin/jpda_plugin-Swift.h>
-
-@implementation JpdaPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftJpdaPlugin registerWithRegistrar:registrar];
-}
-@end
